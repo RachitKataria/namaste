5f6cbe67c3512392f82349df088e96e94b113d85
Rachit Kataria <rachitka@usc.edu>
2020-08-20 01:13:40 -0700

    Added alarm notification logic

---
+++ namaste-app/public/alarm.js
@@ -0,0 +1,23 @@
+// Change the periodInMinutes property to set time interval
+
+/*global chrome*/
+var alarm = chrome.alarms.create("myAlarm", {
+  delayInMinutes: 0.1,
+  periodInMinutes: 120,
+});
+
+chrome.alarms.onAlarm.addListener(function (alarm) {
+  var notification = chrome.notifications.create(
+    "take-a-yoga-break-notification",
+    {
+      type: "basic",
+      iconUrl: "logo192.png", // Replace with your own image
+
+      // Customize either the notification title or message
+      title: "Take a yoga break!",
+      message: "Refresh your mind and body with a quick stretch!",
+    },
+
+    function () {}
+  );
+});
